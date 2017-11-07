c02bf6750f2d2790315f964834f6f9d8754f039d
Darren Eggenschwiler <deggen@Darrens-MacBook-Pro.local>
2017-11-07 21:07:15 +0000

    fix everything

--- signature_BC.php
+++ signature_BC.php
@@ -9,13 +9,14 @@ $photo = $REQUEST["photo"];
 <div style="border-left: 30px solid rgb(150,211,177); padding-left: 15px; font-family: Helvetica, Arial, sans-serif;">
 <br>
 <font size="4" style="font-size: 18px;"><?php echo $fullname;?></font><br>
-<font size="2" style="font-size: 12px; font-"><i><?php echo $jobtitle;?></i></font><br>
+<font size="2" style="font-size: 12px; font-"><i><?php echo $jobtitle;?></i></font><br><br>
 <table cellpadding="0" cellspacing="0" border="0">
   <tbody>
     <tr>
     <td>
-      <font size="4" style="font-size: 12px; color: grey;">
-      <b><?php echo $mobilenumber;?></b>
+      <font size="4" style="font-size: 14px; color: grey;">
+      <?php echo $mobilenumber;?><br>
+      @deggen
       </font>
     </td>
     </tr>
