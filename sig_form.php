a945dc16e1c8ad12e8ff9a7a46c96f51a7ca9852
Darren Eggenschwiler <deggen@Darrens-MacBook-Pro.local>
2017-11-07 19:41:38 +0000

    init

---
+++ sig_form.php
@@ -0,0 +1,13 @@
+<div style="font-size: 20px;"><form action="signature_BC.php" method="post">
+  fullname:<br>
+    <input type="text" name="fullname"><br>
+  jobtitle:<br>
+  <input type="text" name="jobtitle"><br>
+  mobilenumber:<br>
+  <input type="text" name="mobilenumber"><br>
+  emailaddress front only without the brandcalibre.com:<br>
+  <input type="text" name="emailaddress"><br>
+
+  <input type="submit" value="Submit">
+</form>
+</div>
---
+++ signature_BC.php
@@ -0,0 +1,62 @@
+<?php
+$fullname = $_REQUEST["fullname"];
+$jobtitle = $_REQUEST["jobtitle"];
+$mobilenumber = $_REQUEST["mobilenumber"];
+$emailaddress = $_REQUEST["emailaddress"] . "@brandcalibre.com";
+?>
+
+
+<br><br>
+<table cellpadding="0" cellspacing="0" border="0" style="font-family:Times;width:530px;max-width:600px;background-image:initial;background-position:initial;background-repeat:initial;margin:0px;padding:25px;border:10px solid rgb(150,211,177)">
+  <tbody>
+    <tr>
+      <td colspan="2" style="color:black;font-family:Arial,Helvetica,sans-serif">
+        <font size="4">
+<?php echo $fullname;?></font>
+        <br>
+      </td>
+    </tr>
+    <tr>
+      <td colspan="2" style="color:rgb(51,51,51);font-family:Arial,Helvetica,sans-serif">
+        <i><font size="2">
+<?php echo $jobtitle;?></font><br><font size="2"><br></font></i>
+      </td>
+    </tr>
+    <tr>
+      <td colspan="2" style="color:rgb(51,51,51);font-family:Arial,Helvetica,sans-serif"><strong><font size="2">
+  Brand Calibre</font></strong>
+      </td>
+    </tr>
+    <tr>
+      <td width="20" valign="top" style="vertical-align:top;width:20px;color:rgb(150,211,177);font-family:Arial,Helvetica,sans-serif">
+        <font size="2">t:</font></td><td valign="top" style="vertical-align:top;color:rgb(51,51,51);font-family:Arial,Helvetica,sans-serif">
+        <font size="2">
+  0141 212 6356&nbsp;&nbsp;<span style="color:rgb(150,211,177)">m:&nbsp;</span>
+<?php echo $mobilenumber;?></font>
+      </td>
+    </tr>
+    <tr>
+      <td width="20" valign="top" style="vertical-align:top;width:20px;color:rgb(150,211,177);font-family:Arial,Helvetica,sans-serif">
+        <font size="2">a:</font></td><td valign="top" style="vertical-align:top;color:rgb(51,51,51);font-family:Arial,Helvetica,sans-serif">
+        <font size="2">
+  The Whisky Bond, G4 9SS</font>
+      </td>
+    </tr>
+    <tr>
+      <td width="20" valign="top" style="vertical-align:top;width:20px;color:rgb(150,211,177);font-family:Arial,Helvetica,sans-serif"><font size="2">w:</font>
+      </td>
+      <td valign="top" style="vertical-align:top;color:rgb(51,51,51);font-family:Arial,Helvetica,sans-serif"><font size="2"><a href="http://www.brandcalibre.com" style="color:rgb(29,161,219)" target="_blank">www.brandcalibre.com</a>&nbsp;&nbsp;
+        <span style="color:rgb(150,211,177)">e:&nbsp;</span><a href="mailto:<?php echo $emailaddress;?>@brandcalibre.com" style="color:rgb(29,161,219)" target="_blank">
+<?php echo $emailaddress;?></a>
+            </font>
+      </td>
+    </tr>
+    <tr>
+      <td colspan="3" style="padding-top:15px">
+        <a href="https://uk.linkedin.com/company/brand-calibre-limited" target="_blank"><img src="https://ci6.googleusercontent.com/proxy/sd-sHRnrP-aTMvM8TQ3miqgR8QAJ62U7uHuhuuo_QkVA_L51WEMVE1lG3KJTOFfEiOJb6LeJzTDAODyjdX5AYQsT_C304JudrqqB525F6-Oblq5u8FxVgMr34QYCHmqSjySaFmICJNvlleDPm3vqH-pTVYY=s0-d-e1-ft#https://drive.google.com/a/brandcalibre.com/uc?id=0BzyKkLYhtgDVRFlYZHdyZWFnUlE&amp;export=download" alt="linked in"></a>&nbsp;&nbsp;&nbsp;
+        <a href="https://www.instagram.com/brandcalibre/" target="_blank"><img src="https://ci6.googleusercontent.com/proxy/bmEyYe9jkUfJGTNbqg6kBiGyTvHktpysALDvT44Fmq9hlRg90UGVlYZ8aXlh2NRk7iiI0usL2SMgoUVLM4miKZwwydG1n-RDOAC5JewXxDnEAZR5xyF1-ekN5ByaLXZ_cHpY1ejblBLKDRQKYEKQNYxoNY8=s0-d-e1-ft#https://drive.google.com/a/brandcalibre.com/uc?id=0BzyKkLYhtgDVVXVCWmVqU0tPNDA&amp;export=download" alt="instagram"></a>&nbsp;&nbsp;&nbsp;
+        <a href="https://www.facebook.com/brandcalibreltd/" target="_blank"><img src="https://ci6.googleusercontent.com/proxy/XqpPlBmM6JR3zThGx7hqoeuONb9uGjyC3kULiandldIC_Dk2-fQxiuqbPWtXgSwnkrXm23Olp0AVs2pqFXftVRVC8rEifylNDOB2nH6iHGp_4jxgLoa9M4z2Pb91ZibRV_S4YJtT7ZglY003wQKGZkm_qyQ=s0-d-e1-ft#https://drive.google.com/a/brandcalibre.com/uc?id=0BzyKkLYhtgDVeF9aMEFwM1RNdFk&amp;export=download" alt="facebook"></a>
+      </td>
+    </tr>
+  </tbody>
+</table>
