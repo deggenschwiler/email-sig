38e0cb5e9d323eabd92fcd254dd270b8e429e40a
Darren Eggenschwiler <deggen@Darrens-MacBook-Pro.local>
2017-11-10 09:59:14 +0000

    make small one

--- sig_form.php
+++ sig_form.php
@@ -1,10 +1,14 @@
 <div style="font-size: 20px;"><form action="signature_BC.php" method="post">
-  fullname:<br>
+  <b>fullname (required):</b><br>
     <input type="text" name="fullname"><br>
-  jobtitle:<br>
+  <b>jobtitle (required):</b><br>
   <input type="text" name="jobtitle"><br>
-  mobilenumber:<br>
+  mobilenumber (optional):<br>
   <input type="text" name="mobilenumber"><br>
+  personal instagram (username only eg. deggen) (optional):<br>
+  <input type="text" name="insta"><br>
+  personal linkedin (profile identifier only eg. deggen) (optional):<br>
+  <input type="text" name="linkedin"><br>
 <!--  photo (upload to drive, make it publicly accessable under share settings, then paste url here):<br>
     <input type="url" name="photo"><br> -->
 
--- signature_BC.php
+++ signature_BC.php
@@ -2,13 +2,14 @@
 $fullname = $_REQUEST["fullname"];
 $jobtitle = $_REQUEST["jobtitle"];
 $mobilenumber = $_REQUEST["mobilenumber"];
+$insta = $_REQUEST["insta"];
 $mobilenumber = sprintf("%s %s %s", substr($mobilenumber, 0, 5), substr($mobilenumber, 5, 3), substr($mobilenumber, 8, 3));
 $urlphoto =htmlspecialchars($_REQUEST["photo"]);
 $arrayphoto = explode('?id=', $urlphoto, 2);
 $photo = $arrayphoto[1];
 
 // If mobile number then display it.
-//
+// If personal insta - replace icons
 
 ?>
 <br>
@@ -17,8 +18,8 @@ $photo = $arrayphoto[1];
   <tbody>
     <tr>
     <td>
-      <a href="https://uk.linkedin.com/company/brand-calibre-limited" target="_blank"><img src="https://ci6.googleusercontent.com/proxy/sd-sHRnrP-aTMvM8TQ3miqgR8QAJ62U7uHuhuuo_QkVA_L51WEMVE1lG3KJTOFfEiOJb6LeJzTDAODyjdX5AYQsT_C304JudrqqB525F6-Oblq5u8FxVgMr34QYCHmqSjySaFmICJNvlleDPm3vqH-pTVYY=s0-d-e1-ft#https://drive.google.com/a/brandcalibre.com/uc?id=0BzyKkLYhtgDVRFlYZHdyZWFnUlE&amp;export=download" alt="linked in" style="padding: 2px 0px 3px 0px;"></a><br>
-      <a href="https://www.instagram.com/brandcalibre/" target="_blank"><img src="https://ci6.googleusercontent.com/proxy/bmEyYe9jkUfJGTNbqg6kBiGyTvHktpysALDvT44Fmq9hlRg90UGVlYZ8aXlh2NRk7iiI0usL2SMgoUVLM4miKZwwydG1n-RDOAC5JewXxDnEAZR5xyF1-ekN5ByaLXZ_cHpY1ejblBLKDRQKYEKQNYxoNY8=s0-d-e1-ft#https://drive.google.com/a/brandcalibre.com/uc?id=0BzyKkLYhtgDVVXVCWmVqU0tPNDA&amp;export=download" alt="instagram" style="padding-bottom: 3px;"></a><br>
+      <a href="https://uk.linkedin.com/<?php if($linkedin = ""){ echo 'company/brand-calibre-limited';} else {echo 'in/' . $linkedin;}?>" target="_blank"><img src="https://ci6.googleusercontent.com/proxy/sd-sHRnrP-aTMvM8TQ3miqgR8QAJ62U7uHuhuuo_QkVA_L51WEMVE1lG3KJTOFfEiOJb6LeJzTDAODyjdX5AYQsT_C304JudrqqB525F6-Oblq5u8FxVgMr34QYCHmqSjySaFmICJNvlleDPm3vqH-pTVYY=s0-d-e1-ft#https://drive.google.com/a/brandcalibre.com/uc?id=0BzyKkLYhtgDVRFlYZHdyZWFnUlE&amp;export=download" alt="linked in" style="padding: 2px 0px 3px 0px;"></a><br>
+      <a href="https://www.instagram.com/<?php if($insta = ""){ echo 'brandcalibre';} else {echo $insta;}?>/" target="_blank"><img src="https://ci6.googleusercontent.com/proxy/bmEyYe9jkUfJGTNbqg6kBiGyTvHktpysALDvT44Fmq9hlRg90UGVlYZ8aXlh2NRk7iiI0usL2SMgoUVLM4miKZwwydG1n-RDOAC5JewXxDnEAZR5xyF1-ekN5ByaLXZ_cHpY1ejblBLKDRQKYEKQNYxoNY8=s0-d-e1-ft#https://drive.google.com/a/brandcalibre.com/uc?id=0BzyKkLYhtgDVVXVCWmVqU0tPNDA&amp;export=download" alt="instagram" style="padding-bottom: 3px;"></a><br>
       <a href="https://www.facebook.com/brandcalibreltd/" target="_blank"><img src="https://ci6.googleusercontent.com/proxy/XqpPlBmM6JR3zThGx7hqoeuONb9uGjyC3kULiandldIC_Dk2-fQxiuqbPWtXgSwnkrXm23Olp0AVs2pqFXftVRVC8rEifylNDOB2nH6iHGp_4jxgLoa9M4z2Pb91ZibRV_S4YJtT7ZglY003wQKGZkm_qyQ=s0-d-e1-ft#https://drive.google.com/a/brandcalibre.com/uc?id=0BzyKkLYhtgDVeF9aMEFwM1RNdFk&amp;export=download" alt="facebook"></a>
     </td>
     <td style="border-right: solid 2px rgb(150,211,177)">&nbsp;&nbsp;&nbsp;</td>
