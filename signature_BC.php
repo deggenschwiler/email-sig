ba4b53f761fdeea1d63c10fc478efdc66f637f29
Darren Eggenschwiler <deggen@Darrens-MacBook-Pro.local>
2017-11-10 01:21:31 +0000

    make small one

--- signature_BC.php
+++ signature_BC.php
@@ -9,8 +9,7 @@ $photo = $arrayphoto[1];
 ?>
 <br>
 <br>
-<div style="padding-left: 8px; font-family: Helvetica, Arial, sans-serif;">
-<table cellpadding="0" cellspacing="0" border="0">
+<table cellpadding="0" cellspacing="0" border="0" style="padding-left: 8px; font-family: Helvetica, Arial, sans-serif;">
   <tbody>
     <tr>
     <td>
@@ -20,8 +19,8 @@ $photo = $arrayphoto[1];
     <td>&nbsp;&nbsp;&nbsp;</td>
     <td>
       <font size="4" style="font-size: 16px;"><?php echo $fullname;?></font><br>
-      <font size="2" style="font-size: 12px; padding-bottom: 3px;"><i><?php echo $jobtitle;?></i></font><br>
-      <font size="4" style="font-size: 11px; color: grey;">
+      <font size="2" style="font-size: 12px; padding-bottom: 3px; line-height: 16px;"><i><?php echo $jobtitle;?> | Brand Calibre</i></font><br>
+      <font size="4" style="font-size: 11px; color: grey; line-height: 14px;">
       0141 212 6356 <br>
       <?php echo $mobilenumber;?> <br>
       </font>
@@ -30,29 +29,4 @@ $photo = $arrayphoto[1];
   </tbody>
 </table>
 <br>
-</div>
-<div style="border-left: 24px solid rgb(150,211,177); padding-left: 15px; font-family: Helvetica, Arial, sans-serif;">
-<br>
-<font size="4" style="font-size: 16px;">Brand Calibre</font><br>
-<font size="4" style="font-size: 12px; color: grey;">
-The Whisky Bond<br>
-2 Dawson Road<br>
-G4 9SS<br>
-<br>
-0141 212 6356<br>
-www.brandcalibre.com<br>
-<br>
-</font>
-<table cellpadding="0" cellspacing="0" border="0">
-  <tbody>
-    <tr>
-    <td>
-      <a href="https://uk.linkedin.com/company/brand-calibre-limited" target="_blank"><img src="https://ci6.googleusercontent.com/proxy/sd-sHRnrP-aTMvM8TQ3miqgR8QAJ62U7uHuhuuo_QkVA_L51WEMVE1lG3KJTOFfEiOJb6LeJzTDAODyjdX5AYQsT_C304JudrqqB525F6-Oblq5u8FxVgMr34QYCHmqSjySaFmICJNvlleDPm3vqH-pTVYY=s0-d-e1-ft#https://drive.google.com/a/brandcalibre.com/uc?id=0BzyKkLYhtgDVRFlYZHdyZWFnUlE&amp;export=download" alt="linked in"></a>&nbsp;&nbsp;&nbsp;
-      <a href="https://www.instagram.com/brandcalibre/" target="_blank"><img src="https://ci6.googleusercontent.com/proxy/bmEyYe9jkUfJGTNbqg6kBiGyTvHktpysALDvT44Fmq9hlRg90UGVlYZ8aXlh2NRk7iiI0usL2SMgoUVLM4miKZwwydG1n-RDOAC5JewXxDnEAZR5xyF1-ekN5ByaLXZ_cHpY1ejblBLKDRQKYEKQNYxoNY8=s0-d-e1-ft#https://drive.google.com/a/brandcalibre.com/uc?id=0BzyKkLYhtgDVVXVCWmVqU0tPNDA&amp;export=download" alt="instagram"></a>&nbsp;&nbsp;&nbsp;
-      <a href="https://www.facebook.com/brandcalibreltd/" target="_blank"><img src="https://ci6.googleusercontent.com/proxy/XqpPlBmM6JR3zThGx7hqoeuONb9uGjyC3kULiandldIC_Dk2-fQxiuqbPWtXgSwnkrXm23Olp0AVs2pqFXftVRVC8rEifylNDOB2nH6iHGp_4jxgLoa9M4z2Pb91ZibRV_S4YJtT7ZglY003wQKGZkm_qyQ=s0-d-e1-ft#https://drive.google.com/a/brandcalibre.com/uc?id=0BzyKkLYhtgDVeF9aMEFwM1RNdFk&amp;export=download" alt="facebook"></a>
-    </td>
-    </tr>
-  </tbody>
-</table>
-<br>
-</div>
+?>
