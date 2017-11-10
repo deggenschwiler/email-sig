a8047079bf34183c044e4724518196c20e376f48
Darren Eggenschwiler <deggen@Darrens-MacBook-Pro.local>
2017-11-10 01:39:15 +0000

    make small one

--- sig_form.php
+++ sig_form.php
@@ -4,9 +4,9 @@
   jobtitle:<br>
   <input type="text" name="jobtitle"><br>
   mobilenumber:<br>
-  <input type="text" name="mobilenumber"><br>
+<!--  <input type="text" name="mobilenumber"><br>
   photo (upload to drive, make it publicly accessable under share settings, then paste url here):<br>
-    <input type="url" name="photo"><br>
+    <input type="url" name="photo"><br> -->
 
   <input type="submit" value="Submit">
 </form>
